c5da11e0039d85d570b26fd771f394fa2dd16b97
Glitch (tame-ostrich) <none>
2018-04-16 10:54:02 -0500

    part two

--- src/components/interest-calculator.js
+++ src/components/interest-calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setPrincipal, setInterest, setYears } from '../actions';
 
 // Connect this component
 function InterestCalculator(props) {
@@ -9,17 +10,17 @@ function InterestCalculator(props) {
       <div className="form-group">
         <label htmlFor="principal">Principal ($)</label>
         <input type="number" id="principal" value={props.principal}
-          min="0" />
+          min="0" onChange={e => props.dispatch(setPrincipal(e.target.value))} />
       </div>
       <div className="form-group">
         <label htmlFor="interest">Interest rate (%)</label>
         <input type="number" id="interest" value={props.interest}
-          min="0" max="100" step="0.1" />
+          min="0" max="100" step="0.1" onChange={e => props.dispatch(setInterest(e.target.value))} />
       </div>
       <div className="form-group">
         <label htmlFor="years">Years</label>
         <input type="number" id="years" value={props.years}
-          min="0" max="100" />
+          min="0" max="100" onChange={e => props.dispatch(setYears(e.target.value))} />
       </div>
       <div className="form-group">
         <label htmlFor="total">Total</label>
@@ -36,13 +37,13 @@ InterestCalculator.defaultProps = {
   total: 0
 };
 
-function mapStateToProps(state) {
+export const mapStateToProps = (state) => {
   return {
     principal: state.principal,
     interest: state.interest,
     years: state.years,
     total: state.principal * Math.pow(1 + state.interest / 100, state.years)
   };
-}
+};
 
 export default connect(mapStateToProps)(InterestCalculator);

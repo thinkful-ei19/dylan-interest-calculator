79eb580cb23cba576fcb0edda8578d1db8939b9c
Glitch (tame-ostrich) <none>
2018-04-16 10:39:01 -0500

    part one

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import InterestCalculator from './components/interest-calculator';
 import './index.css';
 
 ReactDOM.render(
-    <InterestCalculator />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <InterestCalculator />
+  </Provider>,
+  document.getElementById('root')
 );
